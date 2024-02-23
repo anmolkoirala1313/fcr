2602ee6157c092ab057ba845f5d6b5e42d3be001
anmolko <anmol@canosoft.com.np>
2024-02-23 20:47:19 +0545

    gallery update

--- resources/views/frontend/page/includes/gallery.blade.php
+++ resources/views/frontend/page/includes/gallery.blade.php
@@ -11,12 +11,14 @@
         </div>
         <div class="row mt-20">
             @foreach($element->pageSectionGalleries as $index=>$gallery)
-                <div class="magnific-img mt-2">
-                    <a class="image-popup-vertical-fit"
-                       href="{{ asset(galleryImagePath('section_element').$gallery->resized_name) }}" title="">
-                        <img src="{{ asset(galleryImagePath('section_element').$gallery->resized_name) }}"
-                             class="bora-16 {{ @$page_detail->slug=='legal-document' || @$page_detail->slug=='legal-documents' || @$page_detail->slug=='sample-documents' || @$page_detail->slug=='sample-document' ? '':'image-dimension' }}" alt="" />
-                    </a>
+                <div class="col-4">
+                    <div class="magnific-img mt-2">
+                        <a class="image-popup-vertical-fit"
+                           href="{{ asset(galleryImagePath('section_element').$gallery->resized_name) }}" title="">
+                            <img src="{{ asset(galleryImagePath('section_element').$gallery->resized_name) }}"
+                                 class="bora-16 {{ @$data['row']->slug=='legal-document' || @$data['row']->slug=='legal-documents' || @$data['row']->slug=='sample-documents' || @$data['row']->slug=='sample-document' ? 'w-100':'image-dimension' }}" alt="" />
+                        </a>
+                    </div>
                 </div>
             @endforeach
         </div>
